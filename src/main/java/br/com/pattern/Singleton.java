e3e8784853f25d24ee6d041fffeb1ad401950c46
leandroandrade <mandrak.andrade@gmail.com>
2014-09-10 10:10:27 -0300

    Implementação DP singleton.

--- src/main/java/br/com/pattern/Singleton.java
+++ src/main/java/br/com/pattern/Singleton.java
@@ -2,4 +2,28 @@ package br.com.pattern;
 
 public class Singleton {
 
+	private static String instancia = null;
+
+	private Singleton() {
+
+	}
+
+	public static String getUnicaInstanciaString() {
+		if (instancia == null) {
+			instancia = new String("Apenas uma instancia.");
+		}
+		return instancia;
+	}
+
+	public static void main(String[] args) {
+		String value1 = Singleton.getUnicaInstanciaString();
+		String value2 = Singleton.getUnicaInstanciaString();
+
+		System.out.println(value1);
+		System.out.println(value2);
+
+		System.out.println(value1 == value2);
+		System.out.println(value1.equals(value2));
+	}
+
 }
