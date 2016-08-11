37f2b77e9f6b6cccb9b1f7e91f1b897c14e62e72
leandro <mandrak.andrade@gmail.com>
2016-08-11 06:49:27 -0300

    Correção

--- src/main/java/me/leandro/designpattern/strategy/investimento/Arrojado.java
+++ src/main/java/me/leandro/designpattern/strategy/investimento/Arrojado.java
@@ -4,9 +4,10 @@ public class Arrojado implements Investimento {
 
 	@Override
 	public double investir(Conta conta) {
-		if (new java.util.Random().nextDouble() <= 0.20) {
+		double escolhido = new java.util.Random().nextDouble();
+		if (escolhido <= 0.20) {
 			return conta.getSaldo() * 0.05;
-		} else if (new java.util.Random().nextDouble() > 0.20 && new java.util.Random().nextDouble() <= 0.30) {
+		} else if (escolhido > 0.20 && escolhido <= 0.30) {
 			return conta.getSaldo() * 0.03;
 		}
 		return conta.getSaldo() * 0.06;
--- src/main/java/me/leandro/designpattern/strategy/investimento/TestaInvestimento.java
+++ src/main/java/me/leandro/designpattern/strategy/investimento/TestaInvestimento.java
@@ -3,7 +3,7 @@ package me.leandro.designpattern.strategy.investimento;
 public class TestaInvestimento {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta(100.0);
+		Conta conta = new Conta(100000.0);
 		RealizadorDeInvestimentos realizadorDeInvestimentos = new RealizadorDeInvestimentos();
 		realizadorDeInvestimentos.realizarInvestimento(conta, new Arrojado());
 	}
